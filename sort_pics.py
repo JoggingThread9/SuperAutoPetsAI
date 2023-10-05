6af7eeac0c83bba868b556394b01bf09b238b7a0
Colin Cannell <92816299+colin-cannell@users.noreply.github.com>
2023-10-04 20:09:11 -0400

    Update sort_pics.py

--- sort_pics.py
+++ sort_pics.py
@@ -16,40 +16,33 @@ for fileName in os.listdir("SAP-pics"):
     screenshot = cv2.cvtColor(screenshot, cv2.COLOR_RGB2BGR)
 
     cv2.namedWindow("screen")
-    # cv2.moveWindow("screen", 700, 0)
+    # cv2.moveWindow("screen", 700, 0.png)
 
     cv2.imshow("screen", screenshot)
 
     cv2.waitKey(1)
 
     locations = input("Enter Animals:\n")
-    locations = locations.split(" ")
+    locations = locations.split(",")
+
+    for i in range(len(locations)):
+        if locations[i][0] == ' ':
+            locations[i] = locations[i][1:]
 
     pets = os.listdir("./cascade_training/positive")
 
-    for i in locations:
-        for j in pets:
-            if i == j:
-                shutil.copyfile(path, f"cascade_training/positive/{i}")
-            else:
-                shutil.copyfile(path, f"cascade_training/negative/{i}")
-    
-    cv2.destroyWindow("screen")
+    for i in pets:
+        if i in locations:
+            try:
+                shutil.copyfile(path, f"cascade_training/positive/{i}/{len(os.listdir(f'''cascade_training/positive/{i}'''))}.png")
+            except:
+                pass
+        else:
+            try:
+                shutil.copyfile(path, f"cascade_training/negative/{i}/{len(os.listdir(f'''cascade_training/negative/{i}'''))}.png")
+            except:
+                pass
 
-    # for pet in pets:
-    #     if pet == ".DS_Store":
-    #         pass
-    #     else:
-    #         if pet in names:
-    #             num = len(os.listdir(f"cascade_training/positive/{pet}"))
-    #             newPath = f"{fileName}({num})"
-    #             shutil.copyfile(path, newPath)
-    #             shutil.move(newPath, f"cascade_training/positive/{pet}")
-    #         else:
-    #             num = len(os.listdir(f"cascade_training/negative/{pet}"))
-    #             newPath = f"{fileName}({num})"
-    #             shutil.copyfile(path, newPath)
-    #             shutil.move(newPath, f"cascade_training/negative/{pet}")
-    #
-    #     os.remove(path)
-    # cv2.destroyWindow("screen")
+    shutil.copyfile(path, f"sorted_pics/{len(os.listdir('sorted_pics'))}.png")
+    os.remove(path)
+    cv2.destroyWindow("screen")
