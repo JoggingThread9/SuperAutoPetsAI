578505e528d70463f8e926d6b2c779f0df27a840
Colin Cannell <92816299+colin-cannell@users.noreply.github.com>
2023-10-05 13:09:14 -0400

    Update remove_duplicate_data.py

--- remove_duplicate_data.py
+++ remove_duplicate_data.py
@@ -1,30 +1,36 @@
+# not working currently
+
 import os
 import numpy as np
 from PIL import Image
 
-for i in os.listdir("cascade_training"):
-    try:
-        for j in os.listdir(f"cascade_training/{i}"):
-            try:
-                for k in os.listdir(f"cascade_training/{i}/{j}"):
-                    try:
-                        filePath = f"cascade_training/{i}/{j}/{k}"
-                        file1 = Image.open(f"cascade_training/{i}/{j}/{k}")
-                        file1 = np.array(file1)
-                        for l in os.listdir(f"cascade_training/{i}/{j}"):
-                            try:
-                                if filePath == f"cascade_training/{i}/{j}/{l}":
-                                    pass
-                                else:
-                                    file2 = Image.open(f"cascade_training/{i}/{j}/{l}")
-                                    file2 = np.array(file2)
-                                    if np.array_equal(file1, file2):
-                                        os.remove(f"cascade_training/{i}/{j}/{l}")
-                            except:
-                                pass
-                    except:
-                        pass
-            except:
-                pass
-    except:
-        pass
+# for i in os.listdir("cascade_training"):
+#     if i != ".DS_Store":
+#         for j in os.listdir(f"cascade_training/{i}"):
+#             if j != ".DS_Store":
+#                 for k in os.listdir(f"cascade_training/{i}/{j}"):
+#                     if k != ".DS_Store":
+#                         filePath = f"cascade_training/{i}/{j}/{k}"
+#                         file1 = Image.open(f"cascade_training/{i}/{j}/{k}")
+#                         file1 = np.array(file1)
+#                         for l in os.listdir(f"cascade_training/{i}/{j}"):
+#                             if l != ".DS_Store":
+#                                 if filePath == f"cascade_training/{i}/{j}/{l}":
+#                                     pass
+#                                 else:
+#                                     file2 = Image.open(f"cascade_training/{i}/{j}/{l}")
+#                                     file2 = np.array(file2)
+#                                     if np.array_equal(file1, file2):
+#                                         os.remove(f"cascade_training/{i}/{j}/{l}")
+
+for i in os.listdir("cascade_training/negative/armadillo"):
+    if i != ".DS_Store":
+        img1 = np.array(Image.open(f"cascade_training/negative/armadillo/{i}"))
+        for j in os.listdir("cascade_training/negative/armadillo"):
+            if j != ".DS_Store":
+                if i != j:
+                    img1 = np.array(Image.open(f"cascade_training/negative/armadillo/{i}"))
+                    img2 = np.array(Image.open(f"cascade_training/negative/armadillo/{j}"))
+                    if np.array_equal(img1, img2):
+                        os.remove(f"cascade_training/negative/armadillo/{j}")
+
